9142b83589fa90952fa645b0601c0e41db5d23c8
joel magana <joelmagana15@gmail.com>
2025-10-31 15:44:47 +0300

    checking where

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -32,3 +32,23 @@ INNER JOIN Property p
     ON b.property_id = p.property_id
 LEFT JOIN Payment pay 
     ON b.booking_id = pay.booking_id;
+
+
+
+EXPLAIN 
+SELECT 
+    b.booking_id,
+    b.start_date,
+    b.end_date,
+    b.total_price,
+    u.first_name,
+    p.name AS property_name,
+    pay.amount
+FROM Booking b
+INNER JOIN User u ON b.user_id = u.user_id
+INNER JOIN Property p ON b.property_id = p.property_id
+LEFT JOIN Payment pay ON b.booking_id = pay.booking_id
+ORDER BY b.created_at DESC;
+
+
+SELECT first_name from users WHERE u.email = 'joelmagana@str.com' AND role = 'host';
