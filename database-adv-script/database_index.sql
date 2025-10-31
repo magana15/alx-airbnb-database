275fd77507fb6600a79660c196cd39cb4f35766c
joel magana <joelmagana15@gmail.com>
2025-10-31 15:54:59 +0300

    checking subqueries

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -16,3 +16,8 @@ CREATE INDEX idx_booking_user_id ON Booking(user_id);
 CREATE INDEX idx_booking_property_id ON Booking(property_id);
 CREATE INDEX idx_booking_status ON Booking(status);
 CREATE INDEX idx_booking_start_date ON Booking(start_date);
+
+
+EXPLAIN ANALYZE SELECT * 
+FROM Booking 
+WHERE user_id = '9d2e1a77-2a14-4b09-9f33-1f28b83b4b72';
